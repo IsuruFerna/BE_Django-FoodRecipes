751452b3b85dfb112fc0c24130d47fc551c19d4b
IsuruFerna <isurumadushan.ferna@gmail.com>
2024-07-14 01:19:09 +0200

    creates a view for all the meals

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.core.serializers import serialize
+
+from recipes.models import Meal
 
 # Create your views here.
+def all_meals(request):
+    meals = Meal.objects.all()
+    return JsonResponse(serialize("json", meals), safe=False)
