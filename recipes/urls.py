f48c6a19756ebf0310e54336219229ffd53701a4
IsuruFerna <isurumadushan.ferna@gmail.com>
2024-07-15 12:48:20 +0200

    gets 10 random meals

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.all_meals, name="all_meals")
+    path("", views.all_meals, name="all_meals"),
+    path("random/", views.random_ten_meals, name="random_ten_meals")
 ]
 
--- recipes/views.py
+++ recipes/views.py
@@ -3,9 +3,12 @@ from django.http import JsonResponse
 from django.core.serializers import serialize
 from django.core.paginator import Paginator
 
+import random
+
 from recipes.models import Meal
 
 # gets all the meals with paginator
+# /recipes/?page_num=1&per_page=10
 def all_meals(request):
     meals = Meal.objects.all()
 
@@ -26,3 +29,24 @@ def all_meals(request):
             'total_items': paginator.count
         }
     })
+
+# gets random 10 meals
+# /recipes/random/
+def random_ten_meals(request):
+    meals = Meal.objects.all()
+    meals_amount = meals.count()
+    random_nums = []
+    random_meals = []
+    
+    # controls util it generates 10 unique random numbers
+    while len(random_nums) < 10:
+        generated_num = random.randrange(meals_amount)
+        if not generated_num in random_nums:
+            random_nums.append(generated_num)
+
+            # fills random meal corresponds the generated random number
+            random_meals.append(meals[generated_num])
+
+    return JsonResponse(serialize('json', random_meals), safe=False)
+
+
