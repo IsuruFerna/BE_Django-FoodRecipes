9a63d451ff2837848bc5bdf61cc7027808dd63de
IsuruFerna <isurumadushan.ferna@gmail.com>
2024-07-16 10:38:25 +0200

    handles multiple paramn in serch endpoint

--- recipes/urls.py
+++ recipes/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.all_meals, name="all_meals"),
-    path("random/", views.random_ten_meals, name="random_ten_meals")
+    path("random/", views.random_ten_meals, name="random_ten_meals"),
+    path("search/", views.search_by, name="search"),
 ]
 
--- recipes/views.py
+++ recipes/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.core.serializers import serialize
 from django.core.paginator import Paginator
+from django.db.models import Q
 
 import random
 
-from recipes.models import Meal
+from recipes.models import Category, Meal
 
 # gets all the meals with paginator
 # /recipes/?page_num=1&per_page=10
@@ -30,6 +31,7 @@ def all_meals(request):
         }
     })
 
+
 # gets random 10 meals
 # /recipes/random/
 def random_ten_meals(request):
@@ -50,3 +52,82 @@ def random_ten_meals(request):
     return JsonResponse(serialize('json', random_meals), safe=False)
 
 
+def search_by(request):
+    
+    # required_parm_fields = ("name", "category")
+    # for more improvements with large number of params
+    # https://forum.djangoproject.com/t/parsing-and-validating-query-params-from-get-request/24308
+
+    meal_name = request.GET.get('name')
+    meal_category = request.GET.get('category')
+
+    # returns param response as paginated json
+    def serialized_paginator_response(data):
+        page_number = int(request.GET.get('page_num', 1))
+        items_per_page = int(request.GET.get('per_page', 10))
+
+        paginator = Paginator(data, items_per_page)
+        page_obj = paginator.get_page(page_number)
+
+        serialized_data = serialize('json', page_obj)
+    
+        return JsonResponse({
+            'data': serialized_data,
+            'meta': {
+                'page': page_obj.number,
+                'per_page': items_per_page,
+                'total_pages': paginator.num_pages,
+                'total_items': paginator.count
+            }
+        })
+    
+    if meal_name and meal_category:
+        # filters and gets as a list of categorise according to name similarities
+        categories = Category.objects.filter(strCategory__contains=meal_category)
+
+        # joins multiples querysets into a unique list by provided category name
+        meals = []
+        if categories:
+            for category in categories:
+                meals.extend(Meal.objects.filter(Q(strCategory__strCategory__contains=meal_category) & 
+                                                 Q(strMeal__contains=meal_name)))
+
+            return serialized_paginator_response(meals)
+
+        # when no meal found with the provided name and category
+        if not meals:
+            return JsonResponse({"message": f"No meal found for corresponding name: '{meal_name}' and category: '{meal_category}'"}, status=404)
+        
+        return serialized_paginator_response(meals)
+        
+    
+    if meal_name:
+        meals = Meal.objects.filter(strMeal__contains=meal_name).order_by('strMeal')
+
+        # when thre are no meals to provided name
+        if not meals:
+            print("running inside statement")
+            return JsonResponse({"message": f"No meal found for: {meal_name}"}, status=404)
+        
+        return serialized_paginator_response(meals)
+        
+
+    if meal_category:
+        # filters and gets as a list of categorise according to name similarities
+        categories = Category.objects.filter(strCategory__contains=meal_category)
+
+        # joins multiples querysets into a unique list by provided category name
+        meals = []
+        if categories:
+            for category in categories:
+                meals.extend(Meal.objects.filter(strCategory=category))
+
+            return serialized_paginator_response(meals)
+        
+        # when thre are no categories for provide category
+        return JsonResponse({"message": f"No category found for: {meal_category}"}, status=404)
+    
+    # returns when request doesn't match with any required params
+    return JsonResponse({"error": "Missing query params"}, 
+            status=404
+        )
