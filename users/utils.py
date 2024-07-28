5e74176a1eab774d10760b8961fa83fae2c89eff
IsuruFerna <isurumadushan.ferna@gmail.com>
2024-07-28 21:43:30 +0200

    refactors utils

--- users/utils.py
+++ users/utils.py
@@ -1,5 +1,3 @@
-from faker import Faker
-
 class ResponseUser():
     def __init__(self, id, username, first_name, last_name, email):
         self.id = id
@@ -8,13 +6,3 @@ class ResponseUser():
         self.last_name = last_name
         self.email = email
 
-# generates users
-def user_generator():
-    faker = Faker()
-    return {
-        'email': faker.email(),
-        'first_name': faker.first_name(),
-        'last_name': faker.last_name(),
-        'username': faker.user_name(),
-        'password': faker.password()
-    }
--- utils/utils.py
+++ utils/utils.py
@@ -1,5 +1,7 @@
+from faker import Faker
 from rest_framework.pagination import PageNumberPagination
 
+# paginator response for meal views
 def paginator_response(data, request, Serializer):
     
     # sets pagination using django_rest and it's global settings from the settings.py file under REST_FRAMEWORK list
@@ -8,3 +10,14 @@ def paginator_response(data, request, Serializer):
     serialized_data = Serializer(paginated_data, many=True)
 
     return paginator.get_paginated_response(serialized_data.data)
+
+# generates users for unit test
+def user_generator():
+    faker = Faker()
+    return {
+        'email': faker.email(),
+        'first_name': faker.first_name(),
+        'last_name': faker.last_name(),
+        'username': faker.user_name(),
+        'password': faker.password()
+    }
